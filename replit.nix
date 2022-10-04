84f5ad230dfd26b95556da49de11520eb3d6f82e
xYoshioka <95339427+xYoshioka@users.noreply.github.com>
2022-10-04 09:43:23 +0200

    Add replit.nix

---
+++ replit.nix
@@ -0,0 +1,20 @@
+{ pkgs }: {
+    deps = [
+        #pkgs.cowsay
+        #pkgs.bashInteractive
+        #pkgs.python39Full
+        pkgs.python39
+        pkgs.pipenv
+        pkgs.openssh
+        pkgs.python39Packages.flask
+        pkgs.python39Packages.aiohttp
+        pkgs.python39Packages.requests
+        pkgs.kodiPackages.requests
+        pkgs.python39Packages.cryptography
+        pkgs.python39Packages.pytest
+        pkgs.python39Packages.termcolor
+        pkgs.haskellPackages.termcolor
+
+       
+    ];
+}
