5b016e2406dba77517bd7aaa6cda6737d8a193ff
mheckner <markus.heckner@oth-regensburg.de>
2024-10-04 12:56:53 +0200

    fixed config

--- replit.nix
+++ replit.nix
@@ -1,25 +1,20 @@
 { pkgs }: {
     deps = [
+      pkgs.openssh
+      pkgs.rustc
+      pkgs.pkg-config
+      pkgs.openssl
+      pkgs.libxcrypt
+      pkgs.libiconv
+      pkgs.cargo
         #pkgs.cowsay
         #pkgs.bashInteractive
         #pkgs.python39Full
-        pkgs.python39
         pkgs.pipenv
-        pkgs.openssh
-        pkgs.python39Packages.flask
-        pkgs.python39Packages.aiohttp
-        pkgs.python39Packages.requests
-        pkgs.kodiPackages.requests
-        pkgs.python39Packages.cryptography
-        pkgs.python39Packages.pytest
-        pkgs.python39Packages.termcolor
         pkgs.haskellPackages.termcolor
         pkgs.sqlite
         pkgs.darkhttpd
         #pkgs.python39Packages.flask_sqlalchemy
-        pkgs.python39Packages.sqlparse
-        pkgs.python39Packages.cachelib
-
-       
+    
     ];
 }
